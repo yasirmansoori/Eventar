8f8d0cea6dce15d6eb73920926aa4920dae985bb
Yasir Mansoori <yasir.mansoori000@gmail.com>
2025-02-11 21:56:49 +0530

    Update project configuration and enhance index exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { Eventar } from "./components/calendar-layout";
+import { DEFAULT_BACKGROUND_COLORS } from "./constants/colors";
 import { useEvents } from "./hooks/useEvents";
 import { CalendarEvent } from "./types/calendar";
 import { SpinnerVariant } from "./types/spinner.types";
+import {
+  getEventBackgroundColorClass,
+  getEventColorClass,
+} from "./utils/color-utils";
 
-export { useEvents, Eventar, SpinnerVariant };
+export {
+  useEvents,
+  Eventar,
+  SpinnerVariant,
+  DEFAULT_BACKGROUND_COLORS,
+  getEventColorClass,
+  getEventBackgroundColorClass,
+};
 
 export type { CalendarEvent };
